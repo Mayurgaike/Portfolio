9679c788215ec768cc8055896833b2927c1efa6b
Mayurgaike <mayurgaike486@gmail.com>
2025-01-01 02:14:13 +0530

    Finalize

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
         onHomeClick={() => scrollToSection(homeRef)}
         onAboutClick={() => scrollToSection(aboutRef)}
         onProjectsClick={() => scrollToSection(projectsRef)}
-        onResumeClick={() => window.open('/path-to-resume.pdf', '_blank')} // Replace with actual path
+        onResumeClick={() => window.open('https://drive.google.com/file/d/1dQJSRB63N1Zriiew1Yhi8JIIqZK2Ex-b/view?usp=sharing', '_blank')} // Replace with actual path
         onGitHubClick={() => window.open('https://github.com/Mayurgaike', '_blank')} // Replace with your GitHub link
         onLinkedInClick={() => window.open('https://www.linkedin.com/in/mayur-gaike-9304b4289/', '_blank')} // Replace with your LinkedIn link
       />
